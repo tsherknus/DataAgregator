8a951f8deb9f589821e6e0194d900a8946062040
Tyler Sherknus <tylersherknus@gmail.com>
2025-02-06 23:02:50 -0500

    aggregate stock prices by looping through ticker list

--- DailyStockPrices.py
+++ DailyStockPrices.py
@@ -1,29 +1,35 @@
 import yfinance as yf
 import pandas as pd
+import time
 
-ticker = 'TSLA'
-path_to_save_csv = 'DailyStockPriceData/' + ticker + '.csv'
+with open('RUT_1000_tickers.txt', 'r') as file:
+    for ticker in file:
+        ticker = ticker.strip()
+        print(ticker)
+        path_to_save_csv = 'DailyStockPriceData/' + ticker + '.csv'
 
-data = yf.download(tickers=ticker, period='max', interval='1d')
+        data = yf.download(tickers=ticker, period='max', interval='1d')
 
-data.columns = data.columns.get_level_values(0)
+        data.columns = data.columns.get_level_values(0)
 
-data = data.reset_index(drop=False)
+        data = data.reset_index(drop=False)
 
-df = pd.DataFrame(columns=['Date', 'Close', 'High', 'Low', 'Open', 'Volume'], index=[])
+        df = pd.DataFrame(columns=['Date', 'Close', 'High', 'Low', 'Open', 'Volume'], index=[])
 
-data['Close'] = data['Close'].round(2)
-data['High'] = data['High'].round(2)
-data['Low'] = data['Low'].round(2)
-data['Open'] = data['Open'].round(2)
+        data['Close'] = data['Close'].round(2)
+        data['High'] = data['High'].round(2)
+        data['Low'] = data['Low'].round(2)
+        data['Open'] = data['Open'].round(2)
 
-df['Date'] = data['Date']
-df['Close'] = data['Close']
-df['High'] = data['High']
-df['Low'] = data['Low']
-df['Open'] = data['Open']
-df['Volume'] = data['Volume']
+        df['Date'] = data['Date']
+        df['Close'] = data['Close']
+        df['High'] = data['High']
+        df['Low'] = data['Low']
+        df['Open'] = data['Open']
+        df['Volume'] = data['Volume']
 
-df.set_index('Date')
+        df.set_index('Date')
 
-data.to_csv(path_to_save_csv, index=False)
+        data.to_csv(path_to_save_csv, index=False)
+
+        time.sleep(30)
